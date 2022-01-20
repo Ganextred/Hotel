3234498cbee5d3b3f2353ec0e4e8a3045c846015
Rapirys <ilyarotan@gmail.com>
2022-01-20 21:35:17 +0200

    Initial Commit

---
+++ src/main/java/com/example/luxuryhotel/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.luxuryhotel.repository;
+
+import com.example.luxuryhotel.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
