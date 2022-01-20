7e13fb1cd8cace2e526f373095603e4ba44dbfc8
Rapirys <ilyarotan@gmail.com>
2022-01-20 21:21:07 +0200

    Initial Commit

---
+++ src/main/java/com/example/luxuryhotel/LuxuryHotelApplication.java
@@ -0,0 +1,12 @@
+package com.example.luxuryhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxuryHotelApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LuxuryHotelApplication.class, args);
+    }
+
+}
